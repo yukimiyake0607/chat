40dced919fb64ffdc93e62159bd2ee19a89244bf
yukimiyake0607 <yukimiyake38@gmail.com>
2024-06-14 17:19:38 +0900

    add: 各クラスにstaticを用いた変数idを作成
    refs namedRoutesで使用するため

--- lib/main.dart
+++ lib/main.dart
@@ -15,12 +15,12 @@ class FlashChat extends StatelessWidget {
           bodyMedium: TextStyle(color: Colors.black54),
         ),
       ),
-      initialRoute: '/WelcomeScreen',
+      initialRoute: WelcomeScreen.id,
       routes: {
-        '/WelcomeScreen': (context) => WelcomeScreen(),
-        '/LoginScreen': (context) => LoginScreen(),
-        '/RegistrationScreen': (context) => RegistrationScreen(),
-        '/ChatScreen': (context) => ChatScreen(),
+        WelcomeScreen.id: (context) => WelcomeScreen(),
+        LoginScreen.id: (context) => LoginScreen(),
+        RegistrationScreen.id: (context) => RegistrationScreen(),
+        ChatScreen.id: (context) => ChatScreen(),
       },
     );
   }
