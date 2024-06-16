4ca095830bb33cdb09a2ee9160ed6a894d340835
yukimiyake0607 <yukimiyake38@gmail.com>
2024-06-16 22:20:52 +0900

    add: package:animated_text_kit.dartを追加
    refs タイトルにアニメーションを付与するため

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,6 +1,7 @@
 import 'package:chat/screens/login_screen.dart';
 import 'package:chat/screens/registration_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
 
 class WelcomeScreen extends StatefulWidget {
   static String id = 'welcome_screen';
