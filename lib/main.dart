ce79e6abaf7173671f0ea740d8c667d594637078
yukimiyake0607 <yukimiyake38@gmail.com>
2024-06-18 17:07:06 +0900

    add: Firebaseのインストール

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,17 @@ import 'screens/welcome_screen.dart';
 import 'screens/login_screen.dart';
 import 'screens/registration_screen.dart';
 import 'screens/chat_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() => runApp(FlashChat());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(FlashChat());
+}
 
 class FlashChat extends StatelessWidget {
   @override
