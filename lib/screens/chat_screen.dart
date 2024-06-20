531e30d3d656f8777b478b44eeaaaa684d457d9c
yukimiyake0607 <yukimiyake38@gmail.com>
2024-06-20 16:00:04 +0900

    add: chat_screenに移動したあとのinitStateを追加
    refs ユーザーが認識されていればチャット画面が表示され、されていなければ表示されない

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../constants.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class ChatScreen extends StatefulWidget {
   static String id = 'chat_screen';
@@ -8,8 +9,31 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  final _auth = FirebaseAuth.instance;
+  late final loggedInUser;
+
+  @override
+  void initState() {
+    super.initState();
+
+    getCurrentUser();
+  }
+
+  void getCurrentUser() async {
+    final user = await _auth.currentUser;
+    if (user != null) {
+      try {
+        loggedInUser = user;
+        print(loggedInUser.email);
+      } catch (e) {
+        print(e);
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    getCurrentUser();
     return Scaffold(
       appBar: AppBar(
         leading: null,
