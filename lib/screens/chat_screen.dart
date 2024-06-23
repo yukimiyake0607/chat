b11afd95d2234839d7468f86695490e229c76ccb
yukimiyake0607 <yukimiyake38@gmail.com>
2024-06-23 22:56:02 +0900

    style: messagesBubbleをスタイリング

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -148,14 +148,30 @@ class MessageBubble extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(10.0),
-      child: Material(
-        color: Colors.lightBlueAccent,
-        child: Text(
-          '$text from $sender',
-          style: const TextStyle(
-            fontSize: 50,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          Text(
+            sender,
+            style: const TextStyle(fontSize: 12.0, color: Colors.black54),
           ),
-        ),
+          Material(
+            borderRadius: BorderRadius.circular(30.0),
+            elevation: 5.0,
+            color: Colors.lightBlueAccent,
+            child: Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+              child: Text(
+                text,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 15.0,
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
     ;
