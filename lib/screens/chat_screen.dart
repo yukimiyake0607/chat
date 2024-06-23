3c33f2ca775914a03da8082e438e2c1077b37f28
yukimiyake0607 <yukimiyake38@gmail.com>
2024-06-23 23:03:10 +0900

    add: textFieldにcontrollerプロパティを追加
    refs チャットを送信したらテキストフィールドないの文字が消えるようにするため

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -13,6 +13,7 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  final messageTextController = TextEditingController();
   final _auth = FirebaseAuth.instance;
   late User loggedInUser;
   String messageText = '';
@@ -80,6 +81,7 @@ class _ChatScreenState extends State<ChatScreen> {
                 children: <Widget>[
                   Expanded(
                     child: TextField(
+                      controller: messageTextController,
                       onChanged: (value) {
                         messageText = value;
                       },
@@ -88,6 +90,7 @@ class _ChatScreenState extends State<ChatScreen> {
                   ),
                   TextButton(
                     onPressed: () {
+                      messageTextController.clear();
                       _fireStore.collection('messages').add({
                         'text': messageText,
                         'sender': loggedInUser.email,
@@ -114,38 +117,36 @@ class messagesStream extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return StreamBuilder<QuerySnapshot>(
-              stream: _fireStore.collection('messages').snapshots(),
-              builder: (context, snapshot) {
-                if (!snapshot.hasData) {
-                  const Center(
-                    child: CircularProgressIndicator(
-                      backgroundColor: Colors.lightBlueAccent,
-                    ),
-                  );
-                }
+      stream: _fireStore.collection('messages').snapshots(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          const Center(
+            child: CircularProgressIndicator(
+              backgroundColor: Colors.lightBlueAccent,
+            ),
+          );
+        }
 
-                final messages = snapshot.data?.docs;
-                List<MessageBubble> messageBubbles = [];
-                if (messages != null) {
-                  for (var message in messages) {
-                    final messageText = message.data() as Map<String, dynamic>;
-                    final text = messageText['text'];
-                    final sender = messageText['sender'];
-                    final messageBubble =
-                        MessageBubble(text: text, sender: sender);
-                    messageBubbles.add(messageBubble);
-                  }
-                }
-                return Expanded(
-                  child: ListView(
-                    padding:
-                        EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
-                    physics: AlwaysScrollableScrollPhysics(),
-                    children: messageBubbles,
-                  ),
-                );
-              },
-            );
+        final messages = snapshot.data?.docs;
+        List<MessageBubble> messageBubbles = [];
+        if (messages != null) {
+          for (var message in messages) {
+            final messageText = message.data() as Map<String, dynamic>;
+            final text = messageText['text'];
+            final sender = messageText['sender'];
+            final messageBubble = MessageBubble(text: text, sender: sender);
+            messageBubbles.add(messageBubble);
+          }
+        }
+        return Expanded(
+          child: ListView(
+            padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
+            physics: AlwaysScrollableScrollPhysics(),
+            children: messageBubbles,
+          ),
+        );
+      },
+    );
   }
 }
 
